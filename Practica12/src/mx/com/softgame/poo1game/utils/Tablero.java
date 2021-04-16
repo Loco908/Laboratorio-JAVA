a36fa756543a9e0790c78b297bac63303738043e
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-04-16 09:18:11 -0500

    subida 5

--- Practica12/src/mx/com/softgame/poo1game/utils/Tablero.java
+++ Practica12/src/mx/com/softgame/poo1game/utils/Tablero.java
@@ -10,13 +10,15 @@ public class Tablero {
         this.idx = idx;
     }
     public static void addPersonaje(Personaje p) throws TheException {
-        System.out.println("No es posible insertar el personaje"+ idx);
+        if(idx){
 
-        boolean temp = false;
-        if((idx+ 1)>= 0 && (idx+1) <MAX_LINE){
-            idx++;
-            personajes[idx]=p;
+        }else{
+            throw new TheException("No es posible insertar el personaje", idx)
         }
+
+
+            
+        boolean temp = false;
     }
     public static void delPersonaje(Personaje p)throws TheException{
         boolean temp = false;
