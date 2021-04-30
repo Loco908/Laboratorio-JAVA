5d640e37619e95d1f4f45afe61f7739707529f5b
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-04-30 09:48:31 -0500

    subida7

--- Practica14/src/mx/com/softgame/poo1game/pruebas/PruebaIO.java
+++ Practica14/src/mx/com/softgame/poo1game/pruebas/PruebaIO.java
@@ -1,26 +1,29 @@
 package mx.com.softgame.poo1game.pruebas;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.File;
-import mx.com.softgame.poo1game.personajes.Personaje.*;
+import java.io.FileReader;
+
+import mx.com.softgame.poo1game.personajes.Personaje;
 
 public class PruebaIO{
     public static void main(String[] args) {
         String path = System.getProperty("user.home");
-        Scanner file = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Escriba el archivo:");
-        String fli = file.nextLine();
-
-        //Quita los espacio que tiene de mas.
-        File.trim();
+        String fli = sc.nextLine();
         path += System.getProperty("file.separator") + fli.trim();
-
-        File myfFile = new File(path);
+        File archivo = new File(path);
         System.out.println(path);
-        
-        if(myFile.exists()) {
+
+        if(archivo.exists()) {
             System.out.println("Si Existe");
-            ArrayList <Personaje> personajes = new ArrayList<Personaje>();   
+            ArrayList <Personaje> personajes = new ArrayList<Personaje>();
+            try{
+                FileReader fr = new FileReader(archivo);
+                BufferedReader br = new BufferedReader(archivo);
+            }   
         }
     }
 }
