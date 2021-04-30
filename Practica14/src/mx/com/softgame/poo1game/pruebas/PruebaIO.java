6882e8a194a268ae629bd43882d7625f6f9dfe67
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-04-30 09:01:11 -0500

    Subida5

---
+++ Practica14/src/mx/com/softgame/poo1game/pruebas/PruebaIO.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class PruebaIO{
+    public static void main(String[] args) {
+        String path = System.getProperty("user.home");
+        Scanner file = new Scanner(System.in);
+        System.out.println("Escriba el archivo:");
+        String p = file.nextLine();
+
+
+
+
+    }
+}
