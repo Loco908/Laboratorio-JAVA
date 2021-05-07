7d05d9dee978952599662dea6ad2eb1912fee763
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-05-07 07:59:29 -0500

    subida 3
    Atributos

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -1,3 +1,17 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class VentanaPrincipal{
-    
+    JFrame f;
+    JLabel IbiFil;
+    JTextField txFil;
+    JButton btnOpen;
+    JTextArea txtCont;
+    JLabel IbiN;
+    JLabel IbLeidos;
+    JButton btnExit;
+
 }
