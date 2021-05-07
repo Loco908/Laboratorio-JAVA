dbf5eb9a1b3a57c9369fd5d82609bf92cc69e5f8
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-05-07 08:29:46 -0500

    subida4

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -6,12 +6,22 @@ import javax.swing.JTextField;
 
 public class VentanaPrincipal{
     JFrame f;
-    JLabel IbiFil;
-    JTextField txFil;
+    JLabel IbiFile;
+    JTextField txFile;
     JButton btnOpen;
-    JTextArea txtCont;
+    JTextArea txtContenido;
     JLabel IbiN;
     JLabel IbLeidos;
     JButton btnExit;
 
+    public VentanaPrincipal(){
+        f = new JFrame ("Practica 14");
+        IbiFile = new JLabel("Nombre del archivo:");
+        txFile = new JTextField(20);
+        btnOpen = new JButton("Abrir archivo");
+        txtContenido = new JTextArea(int 30, int 40);
+        IbLeidos = new JLabel("Archivos leidos");
+        IbiN = new JLabel(CERO 0);
+        btnExit = new JButton("Salir");
+    }
 }
