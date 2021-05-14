796b13d72678ba8b1ad528f11ad9da1d377616d3
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-05-14 10:47:04 -0500

    Subida 13

--- Practica16/src/mx/com/softgame/poo1game/ventanas/VentanaHilo.java
+++ Practica16/src/mx/com/softgame/poo1game/ventanas/VentanaHilo.java
@@ -3,6 +3,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import java.awt.GridLayout;
 
 public class VentanaHilo extends JFrame {
     JMenuBar menuBar;
@@ -26,15 +27,44 @@ public class VentanaHilo extends JFrame {
 
     public void initComponents(){
         this.setLayout(new GridLayout(4,1));
+
         menuBar = new JMenuBar();
         this.setJMenuBar(menuBar);
+
         mnuArchivo = new JMenu();
         this.setJMenu(mnuArchivo);
         mnuInicio = new JMenuItem();
+
         this.setJMenuItem(mnuInicio);
         mnuSalir = new JMenuItem();
-        this.setJMenuItem(mnuSalir);
 
+        this.setJMenuItem(mnuSalir);
+        IbiA = new JLabel();
+        
+        this.add(IbiA);
+        this.add(IbiB);
+        this.add(IbiC);
+        this.add(IbiD);
+
+        mnuSalir.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                dispose();
+               
+            }
+        }); 
+
+        mnuInicio.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                iniciarHilos();
+            }
+        }); 
+
+        this.setSize(400, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    }
+
+    public VentanaHilo(){
+        initComponents();
     }
     
 }
