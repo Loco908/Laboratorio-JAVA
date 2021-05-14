b6e268ebeda4f8d1ae3b66bda41eafe12cd521a1
Loco908 <78123331+Loco908@users.noreply.github.com>
2021-05-14 08:00:40 -0500

    Subida 2

--- Practica16/src/mx/com/softgame/poo1game/Hilos/Hilo.java
+++ Practica16/src/mx/com/softgame/poo1game/Hilos/Hilo.java
@@ -1,3 +1,11 @@
-public class Hilo {
-    
+import javax.swing.JLabel;
+
+public class Hilo extends Thread {
+    JLabel etiqueta;
+
+    public Hilo(String name, JLabel etiqueta) {
+        this.name = name;
+        this.etiqueta = etiqueta;
+        
+    }
 }
